ddd7d83cfcdcc624ab085c6170a042302dc1f663
mustafagunes <gunes149@gmail.com>
2019-10-18 17:47:36 +0300

    added example for fonts

--- SwiftGen-Example/ViewController.swift
+++ SwiftGen-Example/ViewController.swift
@@ -12,17 +12,27 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var segueButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Localization string
         titleLabel.text = L10n.HomeVC.Welcome.title
-        //titleLabel.textColor = Asset.Colors.khakiGreen.color
+        
+        // Color
         titleLabel.textColor = ColorName.firstColor.color
+        
+        // Image
         imageView.image = Asset.Image.image.image
+  
+        // Font
+        titleLabel.font = FontFamily.OpenSans.boldItalic.font(size: 12)
+        segueButton.titleLabel?.font = FontFamily.OpenSans.extrabold.font(size: 20)
     }
     
-    
     @IBAction func showAuthTapped(_ sender: Any) {
+        // Segue
         perform(segue: StoryboardSegue.Main.showAuthStoryboard)
     }
 }
